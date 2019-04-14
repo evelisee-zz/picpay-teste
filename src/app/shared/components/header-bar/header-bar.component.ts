789671dbba4635645cf7f5d6adb4e0dba494a9b1
Eve <evelisevazquez@gmail.com>
2019-04-14 19:00:53 -0300

    Arrumando estilo do scrollbar

--- src/app/shared/components/header-bar/header-bar.component.ts
+++ src/app/shared/components/header-bar/header-bar.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pcp-header-bar',
-  templateUrl: './header-bar.component.html'
+  templateUrl: './header-bar.component.html',
+  styleUrls: ['./header-bar.component.scss'],
 })
 export class HeaderBarComponent implements OnInit {
 
